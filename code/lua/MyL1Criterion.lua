360c81f2c178e1024fe7959cf261ea5c06060f4c
tomldh <ldh624@hotmail.com>
2018-09-07 15:50:01 +0200

    remove invalid label from loss/gradient calculation in cnn

--- code/lua/MyL1Criterion.lua
+++ code/lua/MyL1Criterion.lua
@@ -5,8 +5,17 @@ function MyL1Criterion:__init()
 end
 
 function MyL1Criterion:updateOutput(input, target)
+
    -- loss is the Euclidean distance between predicted and ground truth coordinate, mean calculated over batch
-   self.output = torch.mean(torch.norm(input - target, 2, 2))
+   local dists = torch.norm(input - target, 2, 2)
+   -- set loss to zero if target element is invalid (all 0s)
+   for i=1, target:size(1) do
+    if target[i][1]<1e-8 and target[i][2]<1e-8 and target[i][3]<2e-8 then
+     dists[i]:zero()
+    end
+   end
+   self.output = torch.mean(dists)
+
    return self.output
 end
 
@@ -15,6 +24,14 @@ function MyL1Criterion:updateGradInput(input, target)
    local dists = torch.norm(input - target, 2, 2)
    dists = torch.expand(dists, dists:size(1), 3)
    self.gradInput = torch.cdiv(input-target,dists)
+
+   -- set gradient to zero if target element is invalid (all 0s)
+   for i=1, target:size(1) do
+    if target[i][1]<1e-8 and target[i][2]<1e-8 and target[i][3]<2e-8 then
+     self.gradInput[i]:zero()
+    end
+   end
+
    self.gradInput = torch.div(self.gradInput, dists:size(1))
    return self.gradInput
 end
